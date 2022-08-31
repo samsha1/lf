8bbd4425344a160c1fece269c8e0f7164d4797f4
samrat <samratshakya5@gmail.com>
2022-08-31 08:42:55 +0545

    thread daemon programming

---
+++ StopThread.java
@@ -0,0 +1,61 @@
+class MyThread implements Runnable {
+ 
+    
+    private boolean stop;
+ 
+    private String name;
+    Thread t;
+ 
+    MyThread(String threadname)
+    {
+        name = threadname;
+        t = new Thread(this, name);
+        System.out.println("New thread: " + t);
+        stop = false;
+        t.start(); // Starting the thread
+    }
+ 
+    // execution of thread starts from run() method
+    public void run()
+    {
+        int i = 0;
+        System.out.println("Run is called");
+        while (!stop) {
+            System.out.println(name + ": " + i);
+            i++;
+            try {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e) {
+                System.out.println("Caught:" + e);
+            }
+        }
+        System.out.println(name + " Stopped.");
+    }
+ 
+    // for stopping the thread
+    public void stop()
+    {
+        stop = true;
+    }
+}
+ 
+// Main class
+public class StopThread {
+    public static void main(String args[])
+    {
+        // creating two objects t1 & t2 of MyThread
+        MyThread t1 = new MyThread("First  thread");
+        MyThread t2 = new MyThread("Second thread");
+        try {
+            Thread.sleep(500);
+            t1.stop(); // stopping thread t1
+            t2.stop(); // stopping thread t2
+            Thread.sleep(500);
+        }
+        catch (InterruptedException e) {
+            System.out.println("Caught:" + e);
+        }
+        System.out.println("Exiting the main Thread");
+    }
+}
---
+++ ThreadDaemon.java
@@ -0,0 +1,36 @@
+public class ThreadDaemon {
+    
+    public static void main(String[] args) {
+        new SlaveThread().start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println("Exception handling thread: " + e.getMessage()) ;
+        }
+
+        System.out.println("Main Thread terminates") ;
+    }
+
+}
+
+class SlaveThread extends Thread {
+    
+    public SlaveThread() {
+        setDaemon(true); // when set false, the slaveThread continuous to run. When true (daemon) the slaveThread terminates when the main thread / user defined thread terminates.
+    }
+    
+    public void run() {
+        int count = 0;
+
+        while (true) {
+            System.out.println("Running Slave:  "+count++);
+
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                // handle exception here
+            }
+        }
+    }
+}
